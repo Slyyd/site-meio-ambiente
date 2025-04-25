26875a12101f39278ef389eb38c284dfb5a98d9e
Slyyd <windows7kaue@gmail.com>
2025-04-24 23:15:27 -0300

    Foi feito melhorias sobre o sistema anterior

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,38 +1,64 @@
-import  {retornarQuestaoAtual, carregarQuestoes } from "./views/QuestionHandler.js";
+import  {getCurrentQuestion, passCurrentQuestion } from "./views/QuestionHandler.js";
 
-let questionArray = await carregarQuestoes();
-let questaoAtual = retornarQuestaoAtual(questionArray);
+let spn_num_questao = document.querySelector("#spn_num_questao");   // Span para o número da questão
+let spn_txt_questao = document.querySelector("#spn_txt_questao");   // Span para a pergunta da questão
 
-let spn_num_questao = document.querySelector("#spn_num_questao");
-let spn_txt_questao = document.querySelector("#spn_txt_questao");
+let div_questoes = document.querySelector(".questionDiv")           // Div para as opções das questões
 
-let div_questoes = document.querySelector(".questionDiv")
+let h1_pontos = document.querySelector("#h1_pontos");               // H1 para os pontos
+let h1_dificuldade = document.querySelector("#h1_dificuldade");     // H1 para a dificuldade
 
+let questaoAtual;                                                   // Inicialização da váriavel para poder usar fora da função atualizarPagina
 
+atualizarPagina();                                                // Chama a função pela primeira vez ao iniciar a página
 
-function atualizarFrontEnd() {
-    spn_num_questao.textContent = "1";
+async function atualizarPagina() {
+    let valoresAtualizados = await getCurrentQuestion();
+
+    questaoAtual = valoresAtualizados.currentQuestion;
+
+    if (valoresAtualizados.hasEnded) {
+        console.log("AAAAAAAAAAAAA");
+        
+        // Se retornar verdadeiro, é porque as perguntas acabaram
+        spn_num_questao.textContent = "???"
+        spn_txt_questao.textContent = `Você fez ${valoresAtualizados.pointsCounter} ponto(s)!`;
+        h1_pontos.textContent = "Pontos: " + valoresAtualizados.pointsCounter;
+        h1_dificuldade.textContent = "Dificuldade: ???";
+        div_questoes.innerHTML = "";
+        return;
+    }
+
+    spn_num_questao.textContent = valoresAtualizados.questionCounter + 1;
     spn_txt_questao.textContent = questaoAtual.pergunta;
+    h1_pontos.textContent = "Pontos: " + valoresAtualizados.pointsCounter;
+    h1_dificuldade.textContent = "Dificuldade: " + questaoAtual.nivel;
+    div_questoes.innerHTML = ""; // Limpa a div de questões para acomodar a próxima
 
-    let textFinal = "";
-
-    for (const resposta in questaoAtual.opcoes) {
+    for (const resposta in questaoAtual.opcoes) { // Itera sobre todas as opções, criando um botão para cada e setando um id com o número de cada.
         let newButton = document.createElement("button");
         newButton.textContent = questaoAtual.opcoes[resposta];
+        newButton.id = resposta;
 
-        newButton.addEventListener("click", () => {
-            console.log(questaoAtual.resposta)
-            if (resposta == parseInt(questaoAtual.resposta)) {console.log("Parabéns!")}
-        });
+        newButton.addEventListener("click", botaoQuestao);
 
+        if (resposta == parseInt(questaoAtual.resposta)) {newButton.style.backgroundColor = "green";}
 
         div_questoes.appendChild(newButton);
 
     }
 
+}
 
-    
+function botaoQuestao(e) {
+    // Função para os botões do quiz
+
+    let hasScored;
+    if (parseInt(e.currentTarget.id) == questaoAtual.resposta) {hasScored = true;}
+
+    passCurrentQuestion(hasScored)
+
+    atualizarPagina();
 
 }
 
-atualizarFrontEnd();
--- docs/js/views/QuestionHandler.js
+++ docs/js/views/QuestionHandler.js
@@ -1,38 +1,56 @@
 import Question from "../models/Question.js";
 
-const QuestionFile = "../questions.json";
+const QuestionFile = "../questions.json"; // Caminho do .json
 let QuestionJSON = {};
-let questionCounter = 1;
+let questionCounter = 0; // Contador das questões
+let pointsCounter = 0; // Contador de pontos
+let hasEnded = false;
+
+let currentQuestionList = await fetchJson();
 
 
+export async function getCurrentQuestion() {
+    if (currentQuestionList == null) {currentQuestionList = await fetchJson();}
+    if (currentQuestionList.length - 1 < questionCounter) {
+        hasEnded = true;
+    }
+    let currentQuestion = currentQuestionList[questionCounter]
 
+    return {"currentQuestion" : currentQuestion,// Questão da classe Question
+         "questionCounter" : questionCounter,   // Número atual da questão
+         "pointsCounter" : pointsCounter,       // Pontuação atual
+         "hasEnded" : hasEnded                  // Indica se as perguntas acabaram
+        };
+}
 
-export async function carregarQuestoes() {
-    // Carrega todas as questões
+export function passCurrentQuestion(hasScored) {
+    
+    if (hasScored) {
+        pointsCounter++;
+    }
+    
+    questionCounter++
+
+}
+
+async function fetchJson() {
+
     try {
 
-        const dados = await fetch(QuestionFile);
+        const dados = await fetch(QuestionFile); // Lê o .json que contêm as perguntas
         if (!dados.ok) {throw new Error("Falha no Fetch")}
         QuestionJSON = await dados.json();
-        let questionList = criarClasses(QuestionJSON);
+        let questionList = createClasses(QuestionJSON);
         return questionList;
         
     } catch (err) {
         console.error(err);
     }
-
+    
 }
 
-export function retornarQuestaoAtual(currentJSON) {
-    return currentJSON[questionCounter];
-}
-
-export default function proximaQuestao() {
-    // Passa pra próxima questão
-    questionCounter ++;
-}
-
-function criarClasses(questions) {
+function createClasses(questions) {
+    // Pega as questões, cria várias instâncias da mesma classe e armazena elas em uma lista
     let newQuestionArray = []
 
     for (const question in questions) {
@@ -44,6 +62,31 @@ function criarClasses(questions) {
         )
     }
 
-    return newQuestionArray;
+    return shuffleQuestions(newQuestionArray);
 }
 
+function shuffleQuestions(questionList) {
+    /*
+    Usa o método de Fisher Yates
+    Explicação:
+    https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    */
+
+    let newList = questionList;
+
+    for (let i = newList.length - 1; i > 0; i--) {
+
+        let j = Math.floor(Math.random() * (i + 1));
+        let k = newList[i];
+
+        newList[i] = newList[j];
+        newList[j] = k;
+
+    }
+
+    console.log(newList);
+    return newList; // Retorna a lista aleatória
+
+
+
+}
